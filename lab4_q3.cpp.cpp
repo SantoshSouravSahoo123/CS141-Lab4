6df6885f5eab8ec725fcf74050eb6102cc00bf06
SantoshSouravSahoo123 <42238175+SantoshSouravSahoo123@users.noreply.github.com>
2018-08-22 05:59:36 +0000

    Add files via upload

---
+++ lab4_q3.cpp.cpp
@@ -0,0 +1,14 @@
+// first include the library
+#include<iostream>
+using namespace std;
+int main ()
+	{
+	float C,F,temp;
+//Ask the user to put the temp
+	cout << "\n\n Convert the temp in F into C :\n";
+	cout << " Write the temperature in Farhaneit : " ;
+	cin >> F;
+	C = ((5/9)*F - 32);
+	cout << " The temp in Celsius is : "<< C << endl;
+	return 0;
+	}
